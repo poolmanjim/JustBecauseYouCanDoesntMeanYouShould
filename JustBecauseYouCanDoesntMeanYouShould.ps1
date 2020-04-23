92c39d7cf4adbf51fa394a6182db288b9395fe54
poolmanjim <tyler@poolmanjim.com>
2020-04-23 09:49:10 -0500

    INITIAL
    Script is a proof of concept. Utilizes variations of the following unicode characters for variables, alias, and scriptblocks. It is intended to be wildly confusing and extremely hard to follow. 
    https://www.reddit.com/r/PowerShell/comments/g6k3ta/ugliest_working_code_competition_idea/

---
+++ JustBecauseYouCanDoesntMeanYouShould.ps1
@@ -0,0 +1,86 @@
+<#
+    A - Latin A
+    А - Cryillic A
+    Α - Greek Alpha
+#>
+
+Set-Alias "A" "Set-Alias"
+A "AA" "Select-Object"
+A "AА" "Where-Object"
+A "AΑ" "Write-Host"
+
+A "АA" "ForEach-Object"
+A "АА" "New-Object"
+A "ΑΑ" "Sort-Object"
+
+A "ΑA" "Invoke-Command"
+
+$A = 1
+$А = 100
+$Α = "PSCustomObject"
+$ААА = "System.Collections.ArrayList"
+$AAA = @("A","А","Α")
+$ΑΑΑ = АА $ААА
+
+$AAAA = {
+    Param($A)
+    switch($A)
+    {
+        "A" { 
+            $AА = @{
+                "A" = "A"
+                "А" = "А"
+                "Α" = "Α"
+            }
+        }
+        "А" { 
+            $AА = @{
+                "А" = "A"
+                "Α" = "А"
+                "A" = "Α"
+            }
+        }
+        "Α" { 
+            $AА = @{
+                "Α" = "A"
+                "A" = "А"
+                "А" = "Α"
+            }
+        }
+    }
+    return $AА
+}
+
+$AAAА = {
+    Param($ΑΑΑ)
+    $ΑΑΑ | АA {
+        $AA = $_
+        AΑ $AA | AА Α -eq "A" | AA | ΑΑ
+        AΑ $AA | AА А -eq "А" | AA | ΑΑ
+        AΑ $AA | AА Α -eq "A" | AA | ΑΑ
+    }
+}
+
+$A..$А | АA {
+    $AA = $_
+    $AAA | АA {
+        $AА = ΑA -ScriptBlock $AAAA -ArgumentList $AA
+        $АAA = АА $Α @{$AA = $AА}
+        $null = ($ΑΑΑ += $AAA)
+    }
+}
+
+ΑA -ScriptBlock $AAAА -ArgumentList $ΑΑΑ
+
+
+
+
+<#
+if($true)
+{
+    1..100 | Á {
+
+        Α $A | А A -eq "А" | Α | Ä
+    }
+}
+#>
